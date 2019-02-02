2e846f618a0f08c37cea782f0cc2c66aa1bf851e
harleytater <nathangerhardt@mail.weber.edu>
2019-02-01 17:37:39 -0700

    part one complete

--- GraniteWarehouse/Data/ApplicationDbContext.cs
+++ GraniteWarehouse/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using GraniteWarehouse.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,5 +13,6 @@ namespace GraniteWarehouse.Data
             : base(options)
         {
         }
+        DbSet<ProductTypes> ProductType { get; set; }
     }
 }
